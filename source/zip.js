049de69231389ec8f593a0d948d9721546f1e6e2
ansushina <nastya.sushina@yandex.ru>
2019-09-01 21:25:39 +0300

    style fix

--- source/zip.js
+++ source/zip.js
@@ -10,7 +10,7 @@ const zip = (...objects) => {
     const newObject = {};
     
     for (const obj of objects) {
-        for (const key in obj){
+        for (const key in obj) {
             if ( !(key in newObject) ) {
                 newObject[key] = obj[key];
             }  
